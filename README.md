4988c4b5b8e0bd2dccba6b102667339b126b35e8
hjcasayas <henlyjade.casayas@gmail.com>
2024-06-11 02:12:06 +0800

    style: updated tailwind config based on the figma design system

---
+++ README.md
@@ -0,0 +1,9 @@
+# Recipe Page
+A web development challenge from frontendmentor.io.
+
+## Getting Started
+
+First, run the development server:
+```bash
+docker compose up -d
+
--- web/nextjs/tailwind.config.ts
+++ web/nextjs/tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -7,14 +8,27 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      'dark-charcoal': '#312E2C',
+      'dark-raspberry': '7A284E',
+      'wenge-brown': '#5F564D',
+      'eggshell': '#F3E5D7',
+      'white-coffee': '#E3DDD7',
+      'snow': '#FFF7FB',
+      'white': '#FFFFFF'
     },
+    fontFamily: {
+      outer: ['var(--font-outer), sans-serif'],
+      'young-serif': ['var(--young-serif), sans-serif'],
+    },
+    fontSize: {
+      'heading-l': ['40px', { lineHeight: '100px' }],
+      'heading-m': ['24px', { lineHeight: '100px' }],
+      'heading-s': ['20px', { lineHeight: '100px' }],
+      'body-regular': ['16px', { lineHeight: '100px', fontWeight: 400 }],
+      'body-bold': ['16px', { lineHeight: '100px', fontWeight: 600 }],
+    }
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
